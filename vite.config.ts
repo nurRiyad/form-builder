a12dff37b139251f1b44f67178edc508b5e5ca7f
nurRiyad <asadnurriyad@gmail.com>
2024-08-30 02:30:55 +0600

    Update package
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), dts({ tsconfigPath: './tsconfig.app.json' }), vueDevTools()],
+  plugins: [vue(), dts({ tsconfigPath: './tsconfig.app.json', rollupTypes: true }), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
