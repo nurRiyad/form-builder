323af3b486ea249773f1a88d37bf6c04044f4efd
nurRiyad <asadnurriyad@gmail.com>
2024-08-29 23:06:50 +0600

    Make library mode
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, resolve, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -6,13 +6,31 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, '/lib/main.js'),
+      name: 'FormBuilder',
+      // the proper extensions will be added
+      fileName: 'form-builder'
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
     }
   }
 })
