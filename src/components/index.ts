03a98fc8e2e83aa44ac4eea4d239510e74b77593
nurRiyad <asadnurriyad@gmail.com>
2024-08-31 01:58:29 +0600

    Generate schema from interface
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,4 @@
 import FormBuilder from './FormBuilder.vue'
 import HelloWorld from './HelloWorld.vue'
-import InputText from './base/InputText.vue'
-import CheckBox from './base/CheckBox.vue'
-import TheRadio from './base/TheRadio.vue'
-import SingleStep from './derived/SingleStep.vue'
-import TextArea from './base/TextArea.vue'
 
-export { FormBuilder, HelloWorld, InputText, CheckBox, TheRadio, SingleStep, TextArea }
+export { FormBuilder, HelloWorld }
