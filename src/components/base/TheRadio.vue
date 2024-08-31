12c99999f3feeda7af76cbed6a75cb57e82174e0
nurRiyad <asadnurriyad@gmail.com>
2024-08-31 14:43:27 +0600

    Update radio element
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- src/components/base/TheRadio.vue
+++ src/components/base/TheRadio.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import type { Radio } from '@/types/schema'
-import { onUnmounted, ref, watch } from 'vue'
+import { computed, onUnmounted, ref, watch } from 'vue'
 import lodash from 'lodash'
 
 const props = defineProps<{
@@ -24,14 +24,23 @@ watch(
 onUnmounted(() => {
   props.deleteValue(props.element.schema)
 })
+
+const fOptions = computed(() => {
+  const ops = props.element.options || []
+  return ops.map((op) => {
+    if (typeof op === 'string') {
+      return { name: op, value: op }
+    } else return op
+  })
+})
 </script>
 
 <template>
   <div class="flex flex-col space-y-2">
     <p>{{ element.label }}</p>
-    <div v-for="op in element.options" :key="op" class="space-x-2">
-      <input type="radio" :id="op" :name="element.label" :value="op" v-model="picked" />
-      <label :for="op">{{ op }}</label>
+    <div v-for="op in fOptions" :key="op.value" class="space-x-2">
+      <input type="radio" :id="op.value" :name="element.label" :value="op" v-model="picked" />
+      <label :for="op.value">{{ op.name }}</label>
     </div>
   </div>
 </template>
