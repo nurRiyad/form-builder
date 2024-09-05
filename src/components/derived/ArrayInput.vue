9defb49aabcc294fbecf1c198fe0192e76ebb764
nurRiyad <asadnurriyad@gmail.com>
2024-09-06 01:09:30 +0600

    Add basic html for array item
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- src/components/derived/ArrayInput.vue
+++ src/components/derived/ArrayInput.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import type { ArrayInput } from '@/types/schema'
-import { defineAsyncComponent } from 'vue'
+import { defineAsyncComponent, ref } from 'vue'
 
 const ArrayItem = defineAsyncComponent(() => import('../derived/ArrayItem.vue'))
 
@@ -12,17 +12,30 @@ defineProps<{
   setValue: (path: string, val: any) => void
   deleteValue: (key: string) => void
 }>()
+
+const showForm = ref(false)
 </script>
 
 <template>
   <div>
-    <ArrayItem
-      :elements="ui.elements"
-      :schema="schema"
-      :initial-value="initialValue"
-      :fn="func"
-      :set-value="setValue"
-      :delete-value="deleteValue"
-    />
+    <div class="flex justify-between my-2">
+      <h1>{{ ui.label }}</h1>
+      <button @click="showForm = true" class="px-2 py-1 bg-blue-400">Add new</button>
+    </div>
+    <div>
+      <h1>List 1</h1>
+      <h1>List 2</h1>
+    </div>
+    <div class="my-2 p-4 border flex flex-col space-y-2" v-if="showForm">
+      <ArrayItem
+        :elements="ui.elements"
+        :schema="schema"
+        :initial-value="initialValue"
+        :fn="func"
+        :set-value="setValue"
+        :delete-value="deleteValue"
+      />
+      <button @click="showForm = false" class="px-2 py-1 bg-blue-400">Save</button>
+    </div>
   </div>
 </template>
