03ec9bae480c424ca891de10649bcd6bf96a4546
nurRiyad <asadnurriyad@gmail.com>
2024-09-26 23:58:10 +0600

    Add some upcoming element
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

---
+++ src/components/upcoming/TheRating.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <p>Rating Element</p>
+  </div>
+</template>
---
+++ src/components/upcoming/TheSlider.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <p>Slider Element</p>
+  </div>
+</template>
