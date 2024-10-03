92dd2686191991abe2d52a7dd110b75f6482bee3
mohin7 <csemohin@gmail.com>
2024-10-03 19:25:24 +0600

    Update angle icon
    Signed-off-by: mohin7 <csemohin@gmail.com>

---
+++ src/components/icons/TheAngle.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+withDefaults(defineProps<{ direction: 'left' | 'right' }>(), {
+  direction: 'right'
+})
+</script>
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+    class="size-6"
+    width="16"
+    height="16"
+    :style="direction === 'right' ? 'transform: rotate(0deg)' : 'transform: rotate(-180deg)'"
+  >
+    <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+  </svg>
+</template>
--- src/views/FormList.vue
+++ src/views/FormList.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
 import { allForms } from '@/forms'
-import TheArrow from './../components/icons/TheArrow.vue'
+import TheAngle from './../components/icons/TheAngle.vue'
 </script>
 
 <template>
   <div class="hero is-fullheight">
     <div class="hero-body is-flex is-flex-direction-column is-justify-content-center">
-      <h5>All Available Form</h5>
+      <h5 class="mb-8">All Available Form</h5>
       <div class="is-flex is-flex-direction-column gap-8">
         <RouterLink
-          class="button ac-button is-light is-flex is-align-items-center gap-4 is-uppercase"
+          class="button ac-button is-light gap-4 is-uppercase"
           v-for="f in allForms"
           :key="f"
           :to="`/form/${f}`"
         >
           <span>{{ f }}</span>
-          <span class="width-12 height-12">
-            <TheArrow style="transform: rotate(-90deg)" />
+          <span>
+            <TheAngle />
           </span>
         </RouterLink>
       </div>
