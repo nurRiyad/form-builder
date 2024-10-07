ef4f04aaab2ffd858f1ff03b5c98b2d6c98e586f
mohin7 <mohin@appscode.com>
2024-10-07 18:25:26 +0600

    Update content structure
    Signed-off-by: mohin7 <mohin@appscode.com>

--- src/components/FormBuilder.vue
+++ src/components/FormBuilder.vue
@@ -109,50 +109,37 @@ defineExpose({
 </script>
 
 <template>
-  <div class="mt-70">
-    <div class="container" v-if="isLoading">
-      <div class="columns">
-        <div class="column is-8">
-          <h1>Form file loading</h1>
-        </div>
+  <div>
+    <SingleStep
+      v-if="ui.type === 'single-step-from'"
+      :is-active="true"
+      :ui="ui"
+      :parent-err="updateErr"
+    />
+    <MultiStep
+      v-else-if="ui.type === 'multi-step-form'"
+      :active-step="activeStep"
+      :ui="ui"
+      :parent-err="updateErr"
+    />
+    <h1 v-else>No Proper Form type found</h1>
+    <slot name="custom-form" />
+    <template v-if="!hideFormAction">
+      <div
+        class="is-flex is-justify-content-space-between my-4"
+        v-if="ui.type === 'single-step-from'"
+      >
+        <button @click="handleCancel" class="button ac-button">Cancel</button>
+        <button @click="handleSubmit" class="button ac-button is-primary">Submit</button>
       </div>
-    </div>
-    <div class="container" v-else>
-      <div class="columns is-centered">
-        <div class="column is-8">
-          <SingleStep
-            v-if="ui.type === 'single-step-from'"
-            :is-active="true"
-            :ui="ui"
-            :parent-err="updateErr"
-          />
-          <MultiStep
-            v-else-if="ui.type === 'multi-step-form'"
-            :active-step="activeStep"
-            :ui="ui"
-            :parent-err="updateErr"
-          />
-          <h1 v-else>No Proper Form type found</h1>
-          <slot name="custom-form" />
-          <template v-if="!hideFormAction">
-            <div
-              class="is-flex is-justify-content-space-between my-4"
-              v-if="ui.type === 'single-step-from'"
-            >
-              <button @click="handleCancel" class="button ac-button">Cancel</button>
-              <button @click="handleSubmit" class="button ac-button is-primary">Submit</button>
-            </div>
-            <div class="is-flex is-justify-content-space-between my-4" v-else>
-              <button @click="handleStep('Prev')" class="button ac-button">
-                {{ activeStep <= 0 ? 'Cancel' : 'Previous' }}
-              </button>
-              <button @click="handleStep('Next')" class="button ac-button is-primary">
-                {{ activeStep + 1 >= totalStep ? 'Submit' : 'Next' }}
-              </button>
-            </div>
-          </template>
-        </div>
+      <div class="is-flex is-justify-content-space-between my-4" v-else>
+        <button @click="handleStep('Prev')" class="button ac-button">
+          {{ activeStep <= 0 ? 'Cancel' : 'Previous' }}
+        </button>
+        <button @click="handleStep('Next')" class="button ac-button is-primary">
+          {{ activeStep + 1 >= totalStep ? 'Submit' : 'Next' }}
+        </button>
       </div>
-    </div>
+    </template>
   </div>
 </template>
--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -41,13 +41,28 @@ const handleSubmit = (val: any) => {
 </script>
 
 <template>
-  <div v-if="isLoading">Form Loading</div>
-  <FormBuilder
-    v-else
-    :ui="uiData.ui"
-    :initial-value="uiData.value"
-    :schema="uiData.schema"
-    :logic="uiData.js"
-    @on-submit="handleSubmit"
-  />
+  <div class="mt-70">
+    <div class="container" v-if="isLoading">
+      <div class="columns">
+        <div class="column is-8">
+          <h1>Form file loading</h1>
+        </div>
+      </div>
+    </div>
+    <div class="container" v-else>
+      <div class="columns is-centered">
+        <div class="column is-8">
+          <div v-if="isLoading">Form Loading</div>
+          <FormBuilder
+            v-else
+            :ui="uiData.ui"
+            :initial-value="uiData.value"
+            :schema="uiData.schema"
+            :logic="uiData.js"
+            @on-submit="handleSubmit"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
