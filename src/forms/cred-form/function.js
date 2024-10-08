c8bf3b967ed1990ce8561a03f79c39e62131595c
nurRiyad <asadnurriyad@gmail.com>
2024-10-08 21:09:43 +0600

    Updare cred form logic
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- src/forms/cred-form/function.js
+++ src/forms/cred-form/function.js
@@ -1,3 +1,5 @@
+import { useRoute } from 'vue-router'
+
 export function useFunc(model) {
   const isCredType = ({ params }) => {
     const type = params[1] || ''
@@ -6,5 +8,45 @@ export function useFunc(model) {
     else return false
   }
 
-  return { isCredType }
+  const isUpdateForm = () => {
+    const route = useRoute()
+    return route.path.split('/').pop() === 'edit'
+  }
+
+  function setCredentialType() {
+    const credList = [
+      {
+        provider: 'eks',
+        credType: 'Aws'
+      },
+      {
+        provider: 'aks',
+        credType: 'Azure'
+      },
+      {
+        provider: 'gke',
+        credType: 'GoogleCloud'
+      },
+      {
+        provider: 'digitalocean',
+        credType: 'DigitalOcean'
+      },
+      {
+        provider: 'linode',
+        credType: 'Linode'
+      },
+      {
+        provider: 'rancher',
+        credType: 'Rancher'
+      }
+    ]
+    const route = useRoute()
+    const { query } = route || {}
+    const { provider } = query || {}
+    const cred = credList.find((el) => el.provider === provider)
+    if (cred) return cred.credType
+    else return ''
+  }
+
+  return { isCredType, isUpdateForm, setCredentialType }
 }
